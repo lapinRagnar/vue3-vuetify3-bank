f895dd4461c004e311cc0a23ad98b4fabca94a08
lapinRagnar <lapinragnar@gmail.com>
2022-12-01 16:54:19 +0100

    4 - Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,48 @@
 <template>
-  <div>
-    Navbar
-  </div>
+  <v-app-bar color="transparent">
+    <div>
+      <h4 class="text-white">Bonjour</h4> <br>
+      <p class="text-grey caption mt-n7">Let's manage our save now!</p>
+    </div>
+
+    <v-text-field
+      label="Search what you want here..."
+      variant="filled"
+      bg-color="#1D1C21"
+      prepend-inner-icon="mdi:mdi-magnify"
+      class="ml-5"
+    >
+    </v-text-field>
+
+    <v-spacer></v-spacer>
+
+    <v-btn>
+      <v-icon color="#878A94" icon="far fa-envelope"></v-icon>
+    </v-btn>
+
+    <v-badge
+      bordered
+      color="red"
+      content="2"
+      class="mx-5"
+    >
+      <v-icon color="#878A94">far fa-bell</v-icon>
+    </v-badge>
+
+
+    <v-btn>
+      <v-avatar
+        size="30"
+      >
+        <v-img src="https://Cdn.vuetifyjs.com/images/lists/1.jpg" />
+      </v-avatar>
+    </v-btn>
+
+    <div>
+      <p class="text-green mr-10">lapinRagnar</p>
+    </div>
+
+  </v-app-bar>
 </template>
 
 <script setup>
