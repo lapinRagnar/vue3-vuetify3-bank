7c64dfa420b6cd0c165bb535656aea8ddc51713d
lapinRagnar <lapinragnar@gmail.com>
2022-12-01 22:46:26 +0100

    suite du composant Money

--- src/components/Money.vue
+++ src/components/Money.vue
@@ -20,6 +20,8 @@
 
         </v-toolbar>
 
+        <div style="height: 300px;">place pour le graphe chart-vuejs</div>
+
         <!-- <Scatter
           :chart-options="chartOptions"
           :chart-data="chartData"
@@ -34,6 +36,57 @@
 
         </Scatter> -->
 
+      </v-col>
+
+      <v-col cols="12" sm="4">
+
+        <v-card color="#242426" class="rounded-lg mr-2  w-100">
+
+          <v-toolbar height="400" color="transparent">
+
+            <div class="d-flex flex-column align-center justify-space-around w-100">
+
+              <v-toolbar-title class="mb-10">
+                Investation
+              </v-toolbar-title>
+
+              <v-list lines="two" color="transparent" >
+                <v-list-item
+                  v-for="(file, i) in files"
+                  :key="i"
+                  :title="file.title"
+                  :subtitle="file.subtitle"
+                >
+
+                  <template v-slot:append>
+
+                    <v-avatar rounded="0" :color="file.color">
+                      <v-icon color="file.color">{{ file.icon }}</v-icon>
+                    </v-avatar>
+
+                    <v-list-item-action>
+                      <v-btn>
+                        <v-icon color="grey">fas fa-chevron-right</v-icon>
+                      </v-btn>
+                    </v-list-item-action>
+
+                  </template>
+
+
+
+
+                </v-list-item>
+
+
+
+              </v-list>
+
+
+
+            </div>
+
+          </v-toolbar>
+        </v-card>
       </v-col>
 
     </v-row>
